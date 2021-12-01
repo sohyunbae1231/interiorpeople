6afa47e682ea0454d13579106d431e9a5a4530a7
JJPPH <hjp4449@gmail.com>
2021-12-01 12:59:04 +0900

    no message

--- Back End/server/src/routes/imageRouter.js
+++ Back End/server/src/routes/imageRouter.js
@@ -1,11 +1,80 @@
 // @ts-check
+// cSpell:disable
 
 /** 모듈 */
 const { Router } = require('express')
 const { spawn } = require('child_process')
 
+/** 로그인 관련 */
+const { isLoggedIn, ifIsLoggedIn } = require('../middlewares/authentication')
+
+/** multer 및 AWS 관련 */
+const { multerConfig } = require('../middlewares/multerConfig')
+const { s3 } = require('../aws')
+
+const uploadImage = multerConfig('ml_image_img')
+const uploadTheme = multerConfig('ml_theme_img')
+
+/** 라우터 */
 const imageRouter = Router()
 
+/** 사진 업로드 */
+// TODO : segmentation을 여기서 진행
+imageRouter.post('/upload', ifIsLoggedIn, uploadImage.single('image'), async (req, res) => {
+  // @ts-ignore
+  // const userId = req.user.id
+  if (process.env.NODE_ENV === 'dev') {
+    // @ts-ignore
+    req.file.location = req.file.filename
+  }
+  // * 쿠키에 파일 경로를 저장
+  // @ts-ignore
+  res.cookie('image', req.file.filename, { maxAge: 1000 * 60 * 10 })
+  // * segmetation 진행
+  // TODO : 이미지 경로 변경
+  const segmentation = await spawn(
+    `cd ../../../../Machine Learning && dir && python segmentation.py 
+    --trained_model=weights/yolact_plus_resnet50_cig_indoor_2473_4947_interrupt.pth 
+    --config=yolact_resnet50_cig_indoor_config --score_threshold=0.3 --top_k=15 
+    --image=data/train/images/kor_bedroom1.jpg:data/eval_result/kor_bedroom1.jpg 
+    --display_bbox=False --display_text=False `
+  )
+  let result
+  segmentation.stderr.on('data', (data) => {
+    result = data
+    // eslint-disable-next-line no-console
+    console.log(`stderr: ${data.toString()}`)
+  })
+  segmentation.stdout.on('data', (data) => {
+    result = data
+    // eslint-disable-next-line no-console
+    console.log(`stdout: ${data.toString()}`)
+  })
+  res.status(200).json({ result })
+})
+
+/** 스타일 편집(선택) */
+// TODO : 어떤 것을 어떻게 편집할 것인지
+imageRouter.post('/select-style', ifIsLoggedIn, async (req, res) => {})
+
+/** 원하는 테마 이미지 업로드 */
+imageRouter.post('/upload-style', ifIsLoggedIn, uploadTheme.single('theme'), async (req, res) => {
+  // @ts-ignore
+  // const userId = req.user.id
+  if (process.env.NODE_ENV === 'dev') {
+    // @ts-ignore
+    req.file.location = req.file.filename
+  }
+  // @ts-ignore
+  res.cookie('theme', req.file.filename, { maxAge: 1000 * 60 * 10 })
+  res.status(200).json({ message: 'upload succuss' })
+})
+
+/** 영역 설정 */
+// ! 일단 보류
+imageRouter.post('/select-area', ifIsLoggedIn, async (req, res) => {})
+
+/** 결과 보기 */
 imageRouter.get('/seg', async (req, res) => {
   // * package.json 기준
   // const process = spawn('python', ['./src/routes/test11.py', '--print-number', '5'])
@@ -62,11 +131,5 @@ imageRouter.get('/seg', async (req, res) => {
   }) // 실행 >에러
   res.status(200).json({})
 })
-
-// image segmentation
-imageRouter.post('/seg', async (req, res) => {})
-
-// local style transfer
-imageRouter.post('/seg', async (req, res) => {})
 
 module.exports = { imageRouter }
