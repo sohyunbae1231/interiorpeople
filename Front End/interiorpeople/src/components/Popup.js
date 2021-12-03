f81ac81e58136b0089b84b8063519a224a7b8730
sohyunbae1231 <sht06025@naver.com>
2021-12-03 16:30:53 +0900

    팝업 수정 및 이미지 수정&추가

--- Front End/interiorpeople/src/components/Popup.js
+++ Front End/interiorpeople/src/components/Popup.js
@@ -1,5 +1,7 @@
 import React, { Component, useState, useEffect } from 'react';
 import { confirmAlert } from 'react-confirm-alert';
+import Faq from "react-faq-component";
+import axios from "axios";
 
 import "./StyleEdit.css";
 
@@ -21,17 +23,163 @@ class Popup extends Component {
                   onChange={onChange}
                 />
                 <h3>스타일</h3>
-                <input
-                  type="checkbox"
-                  onChange={onChange}
-                />
-                <h3>색상</h3>
-                <input
-                  type="checkbox"
-                  onChange={onChange}
-                />
+                <div class="style-text">고풍스러운</div>
+                <img
+                    alt=""
+                    src={require("../img/classic/white.jpg").default}
+                    style={{ width: "14%", borderRadius:"5px"}}
+                  />
+                  <img
+                    alt=""
+                    src={require("../img/classic/red.jpg").default}
+                    style={{ marginLeft: "2%", width: "14%", borderRadius:"5px" }}
+                  />
+                  <img
+                    alt=""
+                    src={require("../img/classic/brown.jpg").default}
+                    style={{ marginLeft: "2%", width: "14%", borderRadius:"5px" }}
+                  />
+                  <img
+                    alt=""
+                    src={require("../img/classic/blue.jpg").default}
+                    style={{ marginLeft: "2%", width: "14%", borderRadius:"5px" }}
+                  />
+                  <img
+                    alt=""
+                    src={require("../img/classic/gray.jpg").default}
+                    style={{ marginLeft: "2%", width: "14%", borderRadius:"5px" }}
+                  />
+                  <img
+                    alt=""
+                    src={require("../img/classic/black.jpg").default}
+                    style={{ marginLeft: "2%", width: "14%", borderRadius:"5px" }}
+                  />
+                <div class="style-text">컬러풀한</div>
+                <img
+                    alt=""
+                    src={require("../img/natural/white.jpg").default}
+                    style={{ width: "14%", borderRadius:"5px" }}
+                  />
+                  <img
+                    alt=""
+                    src={require("../img/natural/red.jpg").default}
+                    style={{ marginLeft: "2%", width: "14%", borderRadius:"5px" }}
+                  />
+                  <img
+                    alt=""
+                    src={require("../img/natural/brown.jpg").default}
+                    style={{ marginLeft: "2%", width: "14%", borderRadius:"5px" }}
+                  />
+                  <img
+                    alt=""
+                    src={require("../img/natural/blue.jpg").default}
+                    style={{ marginLeft: "2%", width: "14%", borderRadius:"5px" }}
+                  />
+                  <img
+                    alt=""
+                    src={require("../img/natural/gray.jpg").default}
+                    style={{ marginLeft: "2%", width: "14%", borderRadius:"5px" }}
+                  />
+                  <img
+                    alt=""
+                    src={require("../img/natural/black.jpg").default}
+                    style={{ marginLeft: "2%", width: "14%", borderRadius:"5px" }}
+                  />
+                <div class="style-text">북유럽풍</div>
+                <img
+                    alt=""
+                    src={require("../img/northern_europe/white.jpg").default}
+                    style={{ width: "14%", borderRadius:"5px" }}
+                  />
+                  <img
+                    alt=""
+                    src={require("../img/northern_europe/red.jpg").default}
+                    style={{ marginLeft: "2%", width: "14%", borderRadius:"5px" }}
+                  />
+                  <img
+                    alt=""
+                    src={require("../img/northern_europe/brown.jpg").default}
+                    style={{ marginLeft: "2%", width: "14%", borderRadius:"5px" }}
+                  />
+                  <img
+                    alt=""
+                    src={require("../img/northern_europe/blue.jpg").default}
+                    style={{ marginLeft: "2%", width: "14%", borderRadius:"5px" }}
+                  />
+                  <img
+                    alt=""
+                    src={require("../img/northern_europe/gray.jpg").default}
+                    style={{ marginLeft: "2%", width: "14%", borderRadius:"5px" }}
+                  />
+                  <img
+                    alt=""
+                    src={require("../img/northern_europe/black.jpg").default}
+                    style={{ marginLeft: "2%", width: "14%", borderRadius:"5px" }}
+                  />
+                <div class="style-text">깔끔한</div>
+                <img
+                    alt=""
+                    src={require("../img/modern/white.jpg").default}
+                    style={{ width: "14%", borderRadius:"5px" }}
+                  />
+                  <img
+                    alt=""
+                    src={require("../img/modern/red.jpg").default}
+                    style={{ marginLeft: "2%", width: "14%", borderRadius:"5px" }}
+                  />
+                  <img
+                    alt=""
+                    src={require("../img/modern/brown.jpg").default}
+                    style={{ marginLeft: "2%", width: "14%", borderRadius:"5px" }}
+                  />
+                  <img
+                    alt=""
+                    src={require("../img/modern/blue.jpg").default}
+                    style={{ marginLeft: "2%", width: "14%", borderRadius:"5px" }}
+                  />
+                  <img
+                    alt=""
+                    src={require("../img/modern/gray.jpg").default}
+                    style={{ marginLeft: "2%", width: "14%", borderRadius:"5px" }}
+                  />
+                  <img
+                    alt=""
+                    src={require("../img/modern/black.jpg").default}
+                    style={{ marginLeft: "2%", width: "14%", borderRadius:"5px" }}
+                  />
+                <div class="style-text">빈티지한</div>
+                <img
+                    alt=""
+                    src={require("../img/vintage/white.jpg").default}
+                    style={{ width: "14%", borderRadius:"5px" }}
+                  />
+                  <img
+                    alt=""
+                    src={require("../img/vintage/red.jpg").default}
+                    style={{ marginLeft: "2%", width: "14%", borderRadius:"5px" }}
+                  />
+                  <img
+                    alt=""
+                    src={require("../img/vintage/brown.jpg").default}
+                    style={{ marginLeft: "2%", width: "14%", borderRadius:"5px" }}
+                  />
+                  <img
+                    alt=""
+                    src={require("../img/vintage/blue.jpg").default}
+                    style={{ marginLeft: "2%", width: "14%", borderRadius:"5px" }}
+                  />
+                  <img
+                    alt=""
+                    src={require("../img/vintage/gray.jpg").default}
+                    style={{ marginLeft: "2%", width: "14%", borderRadius:"5px" }}
+                  />
+                  <img
+                    alt=""
+                    src={require("../img/vintage/black.jpg").default}
+                    style={{ marginLeft: "2%", width: "14%", borderRadius:"5px" }}
+                  />
               </div>
-              <button style={{ background: "#203864", color: "white", marginLeft:"70%", marginTop:"25px", width:"22%", borderRadius: "5px"}} onClick={() => {
+              <button style={{ background: "#203864", color: "white", height: "25px", marginLeft:"80%", marginTop:"20px", width:"15%", borderRadius: "5px", border: 0, outline: 0}} onClick={() => {
                 if (popup.state === 'update') onUpdate()
                 else onCreate()
                 onClose()
